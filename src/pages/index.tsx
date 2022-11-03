1a6cdc48e6c25727de6556b784012c18aca63ee4
satish-313 <pradhansatish53@gmail.com>
2022-11-03 22:46:06 +0530

    intial commit impliment trpc and tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <p> Hello env store</p>
+    </div>
+  );
+};
+
+export default Home;
