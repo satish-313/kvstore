0c18548bc350c98e76b3c73746a9aa7580e715f5
satish-313 <pradhansatish53@gmail.com>
2024-07-27 23:23:50 +0530

    delete all

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Header } from "./Header";
-export { default as Layout } from "./Layout";
-export { default as Loading } from "./Loading";
-export { default as Projects } from "./Project";
-export { default as MainModal } from "./Modal"
-export { default as Signup } from "./Signup"
--- src/pages/500.tsx
+++
@@ -1,8 +0,0 @@
-export default function Custom500() {
-  return (
-    <div className="flex flex-col items-center">
-      <div className="p-10" />
-      <h1>500 - Server-side error occurred</h1>
-    </div>
-  );
-}
